2a2e9745e9f0f633f37a17049d6d0f75978039b3
j.art <fajarpratama151205@gmail.com>
2026-03-15 12:03:03 +0700

    update

--- player.gd
+++ player.gd
@@ -22,11 +22,10 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jump_velocity
 
-	# TEST PAKAI ui_ (pasti ada di Godot)
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	print("INPUT DETECTED: ", input_dir)
 
-	var direction = (transform.basis * Vector3(input_dir.x, 0, -input_dir.y)).normalized()
+	var direction = (transform.basis * Vector3(-input_dir.x, 0, -input_dir.y)).normalized()
 
 	if direction:
 		velocity.x = direction.x * speed
